72f26bde1b5540f1c0040eda3576162acf23b0d1
William Ritson <william.ritson@gmail.com>
2018-06-09 15:40:46 -0700

    Refactored tip html code.

--- scripts/requests-map.js
+++ scripts/requests-map.js
@@ -1,26 +1,62 @@
-import {
-    makeLineGraph,
-    makeLineGraphData
-} from "./line-graph.js";
-
+import { makeLineGraph, makeLineGraphData } from "./line-graph.js";
 
 var format = d3.format(",");
 
+function makeTip(name, requests, accounts, provided, approval) {
+    return `
+        <table>
+        <caption>Data Requests</caption>
+        <tr>
+            <th align='left'>Country</th>
+            <td align='center'>:</td>
+            <td align='right'>${name}</td>
+        </tr>
+        <tr>
+            <th align='left'># Requests</th>
+            <td align='center'>:</td>
+            <td align='right'>${requests}</td>
+        </tr>
+        <tr>
+            <th align='left'># Accounts</th>
+            <td align='center'>:</td>
+            <td align='right'>${accounts}</td>
+        </tr>
+        <tr>
+            <th align='left'>Data Provided</th>
+            <td align='center'>:</td>
+            <td align='right'>${provided}</td>
+        </tr>
+        <tr>
+            <th align='left'>Approval Rate</th>
+            <td align='center'>:</td>
+            <td align='right'>${approval}e</td>
+        </tr>
+        </table>
+    `;
+}
+
 // Set tooltips
-var tip = d3.tip()
+var tip = d3
+    .tip()
     .attr("class", "d3-tip")
     .offset([100, 0])
-    .html(function (d) {
-        if (isNaN(d.requests) || (d.requests == 0)) {
-            return "<table><caption>Data Requests</caption><tr><th align='left'>Country</th><td align='center'>" + ":" + "</td><td align='right'>" + d.properties.name + "</td></tr><tr><th align='left'># Requests</th><td align='center'>" + ":" + "</td><td align='right'>" + "No Requests Made" + "</td></tr><tr><th align='left'># Accounts</th><td align='center'>" + ":" + "</td><td align='right'>" + "0" + "</td></tr><tr><th align='left'>Data Provided</th><td align='center'>" + ":" + "</td><td align='right'>" + "0" + "</td></tr><tr><th align='left'>Approval Rt</th><td align='center'>" + ":" + "</td><td align='right'>" + "Not Applicable" + "</td></tr></table>";
-
-        } else if (isNaN(d.rate)) {
-
-            return "<table><caption>Data Requests</caption><tr><th align='left'>Country</th><td align='center'>" + ":" + "</td><td align='right'>" + d.properties.name + "</td></tr><tr><th align='left'># Requests</th><td align='center'>" + ":" + "</td><td align='right'>" + format(d.requests) + "</td></tr><tr><th align='left'># Accounts</th><td align='center'>" + ":" + "</td><td align='right'>" + format(d.accounts) + "</td></tr><tr><th align='left'>Data Provided</th><td align='center'>" + ":" + "</td><td align='right'>" + "0" + "</td></tr><tr><th align='left'>Approval Rt</th><td align='center'>" + ":" + "</td><td align='right'>" + "0%" + "</td></tr></table>";
-
+    .html(function(d) {
+        if (isNaN(d.requests) || d.requests == 0) {
+            return makeTip(
+                d.properties.name,
+                "No Requests Made",
+                "0",
+                "Not Applicable",
+                "Not Applicable"
+            );
         } else {
-            return "<table><caption>Data Requests</caption><tr><th align='left'>Country</th><td align='center'>" + ":" + "</td><td align='right'>" + d.properties.name + "</td></tr><tr><th align='left'># Requests</th><td align='center'>" + ":" + "</td><td align='right'>" + format(d.requests) + "</td></tr><tr><th align='left'># Accounts</th><td align='center'>" + ":" + "</td><td align='right'>" + format(d.accounts) + "</td></tr><tr><th align='left'>Data Provided</th><td align='center'>" + ":" + "</td><td align='right'>" + format(Math.round(d.requests * d.rate / 100)) + "</td></tr><tr><th align='left'>Approval Rt</th><td align='center'>" + ":" + "</td><td align='right'>" + format(Math.round(d.rate)) + "%" + "</td></tr></table>";
-
+            return makeTip(
+                d.properties.name,
+                format(d.requests),
+                format(d.accounts),
+                format(Math.round((d.requests * d.rate) / 100)),
+                Math.round(d.rate) + "%"
+            );
         }
     });
 
@@ -33,18 +69,28 @@ var margin = {
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var color = d3.scaleThreshold()
+var color = d3
+    .scaleThreshold()
     .domain([0, 25, 250, 2500, 25000, 205000])
-    .range(["rgb(150,150,150)", "rgb(254,240,217)", "rgb(253,204,138)", "rgb(252,141,89)", "rgb(227,74,51)", "rgb(179,0,0)"]);
+    .range([
+        "rgb(150,150,150)",
+        "rgb(254,240,217)",
+        "rgb(253,204,138)",
+        "rgb(252,141,89)",
+        "rgb(227,74,51)",
+        "rgb(179,0,0)"
+    ]);
 
-var svg = d3.select("#geomap")
+var svg = d3
+    .select("#geomap")
     .attr("id", "line-graph")
     .attr("width", width)
     .attr("height", height)
     .append("g")
     .attr("class", "map");
 
-var projection = d3.geoNaturalEarth1()
+var projection = d3
+    .geoNaturalEarth1()
     .scale(170)
     .translate([width / 2, height / 1.5]);
 
@@ -60,11 +106,14 @@ function mergeYears(data, startYear, endYear) {
         for (let country of yearData) {
             if (countries.has(country.id)) {
                 let existing = countries.get(country.id);
-                existing["requests"] = +existing["requests"] + +country["requests"];
-                existing["accounts"] = +existing["accounts"] + +country["accounts"];
+                existing["requests"] =
+                    +existing["requests"] + +country["requests"];
+                existing["accounts"] =
+                    +existing["accounts"] + +country["accounts"];
                 existing["percentAccepted"] =
                     (+existing["percentAccepted"] * existing.datapoints +
-                        +country["percentAccepted"]) / (existing.datapoints + 1);
+                        +country["percentAccepted"]) /
+                    (existing.datapoints + 1);
                 existing.datapoints += 1;
                 countries.set(country.id, existing);
             } else {
@@ -77,12 +126,18 @@ function mergeYears(data, startYear, endYear) {
     return Array.from(countries.values());
 }
 
-const defaultGraphCountries = ["United States", "India", "United Kingdom", "Germany", "France", "Canada"];
+const defaultGraphCountries = [
+    "United States",
+    "India",
+    "United Kingdom",
+    "Germany",
+    "France",
+    "Canada"
+];
 const countriesToGraph = new Set();
 
 function getCountriesToGraph() {
-    if (countriesToGraph.size > 0)
-        return countriesToGraph;
+    if (countriesToGraph.size > 0) return countriesToGraph;
     return defaultGraphCountries;
 }
 
@@ -92,11 +147,20 @@ function toggleCountry(requestData, country, yearLow, yearHigh) {
     } else {
         countriesToGraph.add(country);
     }
-    makeLineGraph(makeLineGraphData(requestData, getCountriesToGraph(), yearLow, yearHigh));
+    makeLineGraph(
+        makeLineGraphData(requestData, getCountriesToGraph(), yearLow, yearHigh)
+    );
 }
 
 function setData(geoData, request_data, yearLow, yearHigh) {
-    makeLineGraph(makeLineGraphData(request_data, getCountriesToGraph(), yearLow, yearHigh));
+    makeLineGraph(
+        makeLineGraphData(
+            request_data,
+            getCountriesToGraph(),
+            yearLow,
+            yearHigh
+        )
+    );
 
     var requestsById = {};
     var accountsById = {};
@@ -105,14 +169,14 @@ function setData(geoData, request_data, yearLow, yearHigh) {
 
     var requests = mergeYears(request_data, yearLow, yearHigh);
 
-    requests.forEach(function (d) {
+    requests.forEach(function(d) {
         requestsById[d.id] = +d["requests"];
         accountsById[d.id] = +d["accounts"];
         rateById[d.id] = +d["percentAccepted"];
         names[d.id] = d.country;
     });
 
-    geoData.features.forEach(function (d) {
+    geoData.features.forEach(function(d) {
         d.requests = requestsById[d.id];
         d.accounts = accountsById[d.id];
         d.rate = rateById[d.id];
@@ -126,80 +190,74 @@ function setData(geoData, request_data, yearLow, yearHigh) {
         .attr("class", "map-outline")
         .selectAll("path")
         .data(geoData.features)
-        .enter().append("path")
+        .enter()
+        .append("path")
         .attr("d", path)
-        .style("fill", function (d) {
-            let colorVal = requestsById[d.id] == 0 || isNaN(requestsById[d.id]) ? -1 : requestsById[d.id];
+        .style("fill", function(d) {
+            let colorVal =
+                requestsById[d.id] == 0 || isNaN(requestsById[d.id])
+                    ? -1
+                    : requestsById[d.id];
             return color(colorVal);
         })
 
         // tooltips
-        .on("mouseover", function (d) {
+        .on("mouseover", function(d) {
             tip.show(d);
 
             if (!countriesToGraph.has(names[d.id])) {
-                d3.select(this)
-                    .attr("class", "hovered");
+                d3.select(this).attr("class", "hovered");
             }
         })
-        .on("mouseout", function (d) {
+        .on("mouseout", function(d) {
             tip.hide(d);
 
             if (!countriesToGraph.has(names[d.id])) {
-                d3.select(this)
-                    .attr("class", "map-outline");
+                d3.select(this).attr("class", "map-outline");
             }
         })
-        .on("click", function (d) {
+        .on("click", function(d) {
             if (requestsById[d.id] > 0)
                 toggleCountry(request_data, names[d.id], yearLow, yearHigh);
 
             if (countriesToGraph.has(names[d.id])) {
-                d3.select(this)
-                    .attr("class", "selected");
-
+                d3.select(this).attr("class", "selected");
             } else {
-                d3.select(this)
-                    .attr("class", "map-outline");
+                d3.select(this).attr("class", "map-outline");
             }
         });
 
     svg.append("path")
-        .datum(topojson.mesh(geoData.features, function (a, b) {
-            return a.id !== b.id;
-        }))
+        .datum(
+            topojson.mesh(geoData.features, function(a, b) {
+                return a.id !== b.id;
+            })
+        )
         // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
         .attr("class", "names")
         .attr("d", path);
 
-
-    //legend  
+    //legend
     svg.append("g")
         .attr("class", "legendLinear")
         .attr("transform", "translate(30,330)");
 
     var legendIndex = 0;
-    var legendLinear = d3.legendColor()
+    var legendLinear = d3
+        .legendColor()
         .shapeWidth(45)
         .shapeHeight(10)
         .title("Government Data Requests")
         .orient("vertical")
         .scale(color)
-        .labelFormat((d) => {
-            if (isNaN(d))
-                return 0;
+        .labelFormat(d => {
+            if (isNaN(d)) return 0;
             legendIndex++;
-            if (legendIndex % 2 == 0)
-                return d + 1;
+            if (legendIndex % 2 == 0) return d + 1;
             return d;
-
         });
 
-
-
-    svg.select(".legendLinear")
-        .call(legendLinear);
-
+    svg.select(".legendLinear").call(legendLinear);
 }
 
 queue()
@@ -208,9 +266,7 @@ queue()
     .defer(d3.tsv, "data/google_output/all_google.tsv")
     .await(ready);
 
-
 function ready(error, geoData, facebookRequets, googleRequets) {
-
     document.getElementById("select-facebook").addEventListener("click", () => {
         setData(geoData, facebookRequets, 2013, 2017);
     });
@@ -220,7 +276,4 @@ function ready(error, geoData, facebookRequets, googleRequets) {
     });
 
     setData(geoData, facebookRequets, 2013, 2017);
-
-
-
 }
