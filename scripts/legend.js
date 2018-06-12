fc826dfb24f5272584a327ace109ddcf609a0a00
William Ritson <william.ritson@gmail.com>
2018-06-11 22:00:01 -0700

    Reversed legend

--- scripts/legend.js
+++ scripts/legend.js
@@ -2,7 +2,6 @@ import d3 from "https://dev.jspm.io/d3";
 
 const format = d3.format(",");
 
-
 export class VerticalLegend {
     /**
      * Creates a new legend with a given scale
@@ -19,10 +18,10 @@ export class VerticalLegend {
 
         this.sizes = [];
         for (let i = 0; i < thresholds.length - 1; i++) {
-            let rawSize = thresholds[i + 1] - thresholds[i];
+            let rawSize = Math.abs(thresholds[i + 1] - thresholds[i]);
             this.sizes.push(scaler(rawSize));
         }
-        
+
         let smallestSize = Math.min(...this.sizes);
         this.sizes = this.sizes.map(size => size / smallestSize);
         this.totalSize = this.sizes.reduce((a, b) => a + b);
@@ -30,11 +29,11 @@ export class VerticalLegend {
 
     /**
      * Draws the legend in the given area
-     * 
-     * @param {number} x 
-     * @param {number} y 
-     * @param {number} width 
-     * @param {number} height 
+     *
+     * @param {number} x
+     * @param {number} y
+     * @param {number} width
+     * @param {number} height
      */
     draw(x, y, width, height) {
         this.legend = this.svg
@@ -42,15 +41,9 @@ export class VerticalLegend {
             .attr("transform", `translate(${x}, ${y})`);
 
         let yPos = 0;
-        for (let i = 0; i < this.thresholds.length; i++) {
-            let blockHeight = (this.sizes[i] / this.totalSize) * height;
-
+        for (let i = this.thresholds.length - 1; i >= 0; i--) {
+            let blockHeight = ((this.sizes[i] || 0) / this.totalSize) * height;
             let legendYPadding = i === this.thresholds.length - 1 ? 0 : 5;
-            this.legend
-                .append("text")
-                .attr("x", width + 5)
-                .attr("y", yPos + legendYPadding)
-                .text(format(this.thresholds[i]));
 
             if (i < this.sizes.length) {
                 this.legend
@@ -61,6 +54,11 @@ export class VerticalLegend {
                     .attr("height", blockHeight);
             }
             yPos += blockHeight;
+            this.legend
+                .append("text")
+                .attr("x", width + 5)
+                .attr("y", yPos + legendYPadding)
+                .text(format(this.thresholds[i]));
         }
     }
 }
